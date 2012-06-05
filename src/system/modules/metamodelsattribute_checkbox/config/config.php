cce488bf5f044ec564de84154620c2fbca9bda88
David Maack <maack@men-at-work.de>
2012-06-05 17:38:09 +0200

    Added Attribute Checkbox

---
+++ src/system/modules/metamodelsattribute_checkbox/config/config.php
@@ -0,0 +1,24 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage AttributeText
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   CyberSpectrum, MEN AT WORK
+ * @license    private
+ * @filesource
+ */
+
+$GLOBALS['METAMODELS']['attributes']['checkbox'] = array
+(
+	'class' => 'MetaModelAttributeCheckbox',
+	'image' => 'system/modules/metamodelsattribute_checkbox/html/checkbox.gif'
+);
+
+?>
