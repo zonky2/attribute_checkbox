3da771d54bd683f208e28aaa8327f79c56d42e6d
Mini Model <minimodel@metamodel.me>
2013-06-01 01:51:12 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_checkbox/languages/rm/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodelsattribute_checkbox/languages/rm/tl_metamodel_filtersetting.php
@@ -8,10 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:12:22+02:00
+ * last-updated: 2013-05-13T23:08:13+02:00
  */
 
-
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['0']         = 'Ignorar il filter en il modus da prevista';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['1']         = 'Sche questa chaschetta è activada na vegn quest filter betg applitgà sch\'in utilisader e en la prevista dal front-end ed ha activà l\'opziun \'mussar elements anc betg publitgads\'.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['0']      = 'Lubir da surscriver parameters';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['1']      = 'Sche ti activeschas questa chaschetta pon parameters da filters surscriver questa configuraziun.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['checkbox_published']  = '%s <strong>%s</strong> %s <br /> da l\'attribut <em>%s</em>';
