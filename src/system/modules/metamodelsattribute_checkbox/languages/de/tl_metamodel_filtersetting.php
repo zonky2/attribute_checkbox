9cb687ce6a0b197862082a714b473ac92bb4e2e8
Mini Model <minimodel@metamodel.me>
2013-06-20 20:13:33 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_checkbox/languages/de/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodelsattribute_checkbox/languages/de/tl_metamodel_filtersetting.php
@@ -8,10 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:12:22+02:00
+ * last-updated: 2013-05-13T23:08:13+02:00
  */
 
-
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['0']         = 'Filter nicht in Frontendvorschau nutzen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['1']         = 'Falls ausgewählt wird dieser Filter nicht benutzt wenn ein Benutzer die Frontend-Vorschau nutzt. Die Option "Unveröffentlichte Elemente anzeigen" wird ignoriert.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['0']      = 'Überschreiben erlauben';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['1']      = 'Falls aktiviert können Filterparameter diese Einstellung überschreiben.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['checkbox_published']  = '%s <strong>%s</strong> %s <br /> für Attribut <em>%s</em>';
