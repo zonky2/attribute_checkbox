606c7b4e1e4bf9e5502f534e9695be72acf1642a
Mini Model <minimodel@metamodel.me>
2013-07-01 16:14:32 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_checkbox/languages/fr/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-06-13T00:10:33+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0'] = 'Ignorer le champ publié %s';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1'] = 'Si coché, la condition publiée pour l\'attribut %s ne sera pas appliquée dans le filtre.';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Basculer';
