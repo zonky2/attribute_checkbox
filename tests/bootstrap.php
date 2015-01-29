4bbf390fa7170569238cde34189a9146cb405fc1
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-29 18:39:28 +0100

    Add autoload information for unit tests and bump dependency of contao-toolbox.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,6 @@
  * data in each collection.
  *
  * PHP version 5
- *
  * @package    MetaModels
  * @subpackage Tests
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
@@ -32,5 +31,3 @@ if (
         'php composer.phar install'.PHP_EOL;
     exit(1);
 }
-
-$loader->add('MetaModels\Test', __DIR__);
