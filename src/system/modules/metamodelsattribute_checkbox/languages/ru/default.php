3faeca9980ce9a45b8c2d8d1206d69f49d42b2a2
Mini Model <minimodel@metamodel.me>
2015-04-23 12:13:17 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_checkbox/languages/ru/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-07-10T10:03:59+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0'] = 'Игнорировать опубликованное поле %s';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1'] = 'Если выбрать, опубликованное условие для атрибута %s не будет применяться в фильтре.';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Переключить';
+
---
+++ src/system/modules/metamodelsattribute_checkbox/languages/ru/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-07-10T10:03:59+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['0']         = 'Игнорировать фильтр в режиме предварительного просмотра';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['1']         = 'Если выбрать, фильтр не будет применен, когда пользователь находится в режиме предварительного просмотра внешнего интерфейса и имеет включенную опцию "показать неопубликованные элементы".';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['0']      = 'Разрешить переопределение параметра';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['1']      = 'Если выбрать, этот параметр может переопределить настройки фильтра.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['checkbox_published']  = '%s <strong>%s</strong> %s <br /> в атрибуте <em>%s</em>';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['checkbox_published'] = 'Опубликовать состояние';
+
