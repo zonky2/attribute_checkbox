20fadfdd927d241bc870b1486117bdcc3f8cdb31
Mini Model <minimodel@metamodel.me>
2015-09-18 09:47:34 +0200

    Updated translations from transifex.

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -8,11 +8,13 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:10:49+02:00
+ * last-updated: 2015-07-11T07:53:27+02:00
  */
 
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0'] = 'Veröffentlichungsstatus in Feld %s ignorieren';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1'] = 'Falls ausgewählt wird die Bedingung zur Veröffentlichung für Attribut %s im Filter nicht berücksichtigt.';
 $GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['0']       = 'Wechseln';
-$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Wechseln';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Umschaltung des Status von Attribut %s des Datensatzes.';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['value_0']           = 'Inaktiv';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['value_1']           = 'Aktiv';
 
