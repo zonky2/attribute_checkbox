54db90bb77d5163251254e5907e2dcd81a34799d
Mini Model <minimodel@metamodel.me>
2016-03-14 00:13:29 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_attribute.php
+++ contao/languages/fr/tl_metamodel_attribute.php
@@ -8,10 +8,16 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-07-10T10:03:59+02:00
+ * last-updated: 2015-07-11T04:10:50+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish']['0']      = 'Publication de la case';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish']['1']      = 'Si sélectionné, l\'objet apparaitra dans les listes du frontoffice (vous aurez automatiquement l’œil vert activé.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['checkbox'] = 'Case';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listview']['0']             = 'Ajouter comme case clicable dans la liste du back-office';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listview']['1']             = 'Cochez pour ajouter cette case dans la liste du back-office.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listviewicon']['0']         = 'Icône pour la liste backend.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listviewicon']['1']         = 'Icône lorsque la case est cochée.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listviewicondisabled']['0'] = 'Icône quand la case est décochée.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listviewicondisabled']['1'] = 'Icône quand la case est décochée.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish']['0']              = 'Case à cocher de publication';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish']['1']              = 'Cochez pour attribuer à cette case à cocher la fonction de publication (l\'œil vert dans les listes du back-office). Cette case servira pour le filtre publication.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['checkbox']         = 'Case à cocher';
 
