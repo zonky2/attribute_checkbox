4d8b4c1279ec70bb52f44d09fa361bc26ade0d27
Mini Model <minimodel@metamodel.me>
2017-03-09 12:13:32 +0100

    Updated translations from transifex.

--- contao/languages/it/default.php
+++ contao/languages/it/default.php
@@ -8,11 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:10:50+02:00
+ * last-updated: 2017-03-09T12:13:23+01:00
  */
 
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0'] = 'Ignora campo di pubblicazione %s';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1'] = 'Se si seleziona questo, la condizione di pubblicazione per l\'attributo %s non verrà applicata nel filtro.';
 $GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['0']       = 'Toggle';
-$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Toggle';
 
--- contao/languages/rm/default.php
+++ contao/languages/rm/default.php
@@ -8,11 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:10:50+02:00
+ * last-updated: 2017-03-09T12:13:23+01:00
  */
 
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0'] = 'Ignorar il champ da publicaziu %s';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1'] = 'Sche ti activeschas quai, vegn la cundiziun da publicaziun per l\'attribut %s betg sfurzà en il fiter.';
 $GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['0']       = 'Activar/deactivar';
-$GLOBALS['TL_LANG']['MSC']['metamodelattribute_checkbox']['toggle']['1']       = 'Activar/deactivar';
 
